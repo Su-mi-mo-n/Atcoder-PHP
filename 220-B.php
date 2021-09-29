c271f93192fdb04d803d0a48ba367b0205775b81
Su-mi-mo-n <84580701+Su-mi-mo-n@users.noreply.github.com>
2021-09-29 15:18:33 +0900

    Delete 220-B.php

--- 220-B.php
+++
@@ -1,9 +0,0 @@
-<?php
-$shinsuu = trim(fgets(STDIN));
-$input = explode(' ', trim(fgets(STDIN)));
- 
-$out1 = base_convert($input[0], $shinsuu, 10);
-$out2 = base_convert($input[1], $shinsuu, 10);
- 
-print $out1 * $out2;
-?>
